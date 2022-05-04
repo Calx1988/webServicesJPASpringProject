ce6aa350725260a19583584ea1e57a927db1e3c7
Calx1988 <cassioalexsander@gmail.com>
2022-05-03 21:37:08 -0300

    Primeiro Commit

---
+++ src/main/java/webservices_springboot_jpa/webservices_springboot_jpa/WebservicesSpringbootJpaApplication.java
@@ -0,0 +1,15 @@
+package webservices_springboot_jpa.webservices_springboot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+
+@SpringBootApplication
+public class WebservicesSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webservices_springboot_jpa/webservices_springboot_jpa/WebservicesSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package webservices_springboot_jpa.webservices_springboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
