c6afa704af41b31bfa7a0fca4f1763590a2c5c99
Calx1988 <cassioalexsander@gmail.com>
2022-05-03 22:18:03 -0300

    Inseridas dependencias e criada classe User

--- src/main/java/webservices_springboot_jpa/webservices_springboot_jpa/resources/UserResource.java
+++ src/main/java/webservices_springboot_jpa/webservices_springboot_jpa/resources/UserResource.java
@@ -1,2 +1,15 @@
-package webservices_springboot_jpa.webservices_springboot_jpa.resources;public class UserResource {
+package webservices_springboot_jpa.webservices_springboot_jpa.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import webservices_springboot_jpa.webservices_springboot_jpa.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "Maria", "maria@gmail.com", "519999999", "maria");
+        return ResponseEntity.ok().body(user);
+    }
 }
