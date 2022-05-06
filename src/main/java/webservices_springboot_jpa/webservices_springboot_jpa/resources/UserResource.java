9232b18b2d4eb9967501d3bb921cd8f9e43da8d7
Calx1988 <cassioalexsander@gmail.com>
2022-05-06 18:53:49 -0300

    Uri path to user

--- src/main/java/webservices_springboot_jpa/webservices_springboot_jpa/resources/UserResource.java
+++ src/main/java/webservices_springboot_jpa/webservices_springboot_jpa/resources/UserResource.java
@@ -3,9 +3,11 @@ package webservices_springboot_jpa.webservices_springboot_jpa.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import webservices_springboot_jpa.webservices_springboot_jpa.entities.User;
 import webservices_springboot_jpa.webservices_springboot_jpa.services.UserService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,6 +32,7 @@ public class UserResource {
     @PostMapping(value = "/")
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj=userService.insertUser(obj);
-        return ResponseEntity.ok().body(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
